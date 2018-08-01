62a3eeac13ba506d300b8d1ec956bb9b99d13a8a
湾流—俊俊的Mac <>
2018-08-01 09:30:10 +0800

    新创建的项目呢

---
+++ testPdescDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testPdescDemo
+//
+//  Created by 湾流—俊俊的Mac on 2018/7/31.
+//  Copyright © 2018年 湾流—俊俊的Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testPdescDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testPdescDemo
+//
+//  Created by 湾流—俊俊的Mac on 2018/7/31.
+//  Copyright © 2018年 湾流—俊俊的Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
