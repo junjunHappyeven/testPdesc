83bc260a2c5ae1bc335b6ad4f582fe0d146d6dab
湾流—俊俊的Mac <>
2018-08-01 11:38:44 +0800

    podspec配置完全了

---
+++ testTableView/teseView.h
@@ -0,0 +1,13 @@
+//
+//  teseView.h
+//  testPdescDemo
+//
+//  Created by 湾流—俊俊的Mac on 2018/8/1.
+//  Copyright © 2018年 湾流—俊俊的Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface teseView : UIView
+
+@end
---
+++ testTableView/teseView.m
@@ -0,0 +1,21 @@
+//
+//  teseView.m
+//  testPdescDemo
+//
+//  Created by 湾流—俊俊的Mac on 2018/8/1.
+//  Copyright © 2018年 湾流—俊俊的Mac. All rights reserved.
+//
+
+#import "teseView.h"
+
+@implementation teseView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
